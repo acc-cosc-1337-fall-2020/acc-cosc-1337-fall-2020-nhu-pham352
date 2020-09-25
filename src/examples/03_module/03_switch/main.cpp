1b9ed783b4e17f3afa47f1909cf62ddcae6dc5e4
nhu-pham352 <nhu.pham352@g.austincc.edu>
2020-09-25 15:46:03 -0500

    switch and immediate if

--- src/examples/03_module/03_switch/main.cpp
+++ src/examples/03_module/03_switch/main.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
+#include "switch.h"
 
+using std::cout;	using std::cin;
 int main() 
 {
-	
+	int option, y=0, z=0;
+
+	cout<<"Enter a number: ";
+	cin>>option;
+
+	std::string result = menu(option);
+
+	cout<<result<<"\n";
+
+	option > 5 ? y = 10 : z = 20; //Immediate if
+	cout<<" Y "<<y<<" Z "<<z;
 
 	return 0;
 }
